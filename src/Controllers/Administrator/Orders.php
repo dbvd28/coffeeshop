cdf55d82a4989e0655889f61c516f1b89242bd71
dbvd28 <60490932+dbvd28@users.noreply.github.com>
2025-07-28 22:03:42 -0600

    feat: se agrega funcionalidad a los controladores de order y orders tambien se le agrega la seguridad a ambas pantallas

--- src/Controllers/Administrator/Orders.php
+++ src/Controllers/Administrator/Orders.php
@@ -5,14 +5,17 @@ namespace Controllers\Administrator;
 use Controllers\PrivateController;
 use Dao\Administrator\Orders as ODAO;
 use Views\Renderer;
+use Utilities\Site as Site;
 class Orders extends PrivateController{
     private array $viewData;
     public function __construct(){
         parent::__construct();
         $this->viewData=[];
+         $this->viewData["isUpdateEnabled"]=parent::isFeatureAutorized($this->name."\update");
     }
     public function run():void{
-       // $this->viewData["categories"]=CategoriesDao::getCategories();
+    $this->viewData["pedidos"]=ODAO::getOrders();
+  Site::addLink("public/css/orders2.css");
     Renderer::render("Administrator/orders",$this->viewData);
     }
 }
